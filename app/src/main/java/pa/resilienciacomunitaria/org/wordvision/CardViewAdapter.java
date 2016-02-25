869334b2b2fc4f9652e18b8947a6ce2e5fa60e56
vicgp91 <vicgp_24@hotmail.com>
2016-02-25 17:31:20 -0500

    NUevo

---
+++ app/src/main/java/pa/resilienciacomunitaria/org/wordvision/CardViewAdapter.java
@@ -0,0 +1,41 @@
+package pa.resilienciacomunitaria.org.wordvision;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CardViewAdapter extends ArrayAdapter<PrimerosAuxilios> {
+    private Context context;
+    private ArrayList<PrimerosAuxilios> datos;
+
+
+    public CardViewAdapter(Context context, ArrayList<PrimerosAuxilios> datos) {
+        super(context, R.layout.items_auxilio, datos);
+        this.context = context;
+        this.datos = datos;
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View item = LayoutInflater.from(context).inflate(
+                R.layout.items_auxilio, null);
+				
+        ImageView imagen = (ImageView) item.findViewById(R.id.imgAux);
+        imagen.setImageResource(datos.get(position).getImagen());
+
+        TextView nombre = (TextView) item.findViewById(R.id.tvTituloAux);
+        nombre.setText(datos.get(position).getNombre());
+        return item;
+    }
+
+
+
+}
